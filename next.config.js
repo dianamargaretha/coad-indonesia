6a1f1184228c28c2d9df6061b34cfe076fce176b
diana margaretha <dianatampubolon@gmail.com>
2023-11-16 21:18:35 +0700

    add domain image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost'],
+        domains: ['localhost', 'navajowhite-hedgehog-691446.hostingersite'],
     },
 }
 
