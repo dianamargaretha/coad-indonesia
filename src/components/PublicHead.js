181c30779c11718d29364309b136280b38ed74f1
diana margaretha <dianatampubolon@gmail.com>
2024-01-30 08:43:17 +0700

    feat(update google site)

--- src/components/PublicHead.js
+++ src/components/PublicHead.js
@@ -5,7 +5,7 @@ const PublicHead = ({ title, description }) => {
         <Head>
             <meta charSet="utf-8" />
             <title>{title}</title>
-            <meta name="google-site-verification" content="esnisdAKKwM4vC5LBawCq65SjAVZ2nY5j967JZzGAtE" />
+            <meta name="google-site-verification" content="69YMIGUYzbOr0U6fU9zQjPIiiaa3-GNp4IUUWBm3dDw" />
             <meta name="description" content={description}></meta>
             <link rel="shortcut icon" href="/favicon-32x32.png" />
         </Head>
