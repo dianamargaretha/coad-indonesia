e9ae508cf4fea58fc58ffc0987a58292376d7e3b
diana margaretha <dianatampubolon@gmail.com>
2025-02-16 14:51:41 +0700

    feat(add middleware with path condition)

---
+++ src/middleware.js
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+	if (request.nextUrl.pathname.startsWith('/wordpress/wp-admin')) {
+		return NextResponse.rewrite(new URL('/wordpress/wp-admin', request.url))
+	}
+}
