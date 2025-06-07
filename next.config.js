176ed6476d5a12181638d82a22adf80b486aaef9
diana margaretha <dianatampubolon@gmail.com>
2025-06-07 16:38:20 +0700

    redirect tag

--- next.config.js
+++ next.config.js
@@ -2,7 +2,19 @@
 const nextConfig = {
     images: {
         domains: ['localhost', '195.35.14.168', 'highspeeddoorindonesiacoad.com'],
-    }
+    },
+	async redirects() {
+		return [
+			{
+				source: '/wordpress/tag',
+				destination: '/'
+			},
+			{
+				source: '/wordpress/tag/:slug',
+				destination: '/'
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
