e311b3ce34409747d73698f41fa98990b7327bd6
diana margaretha <dianatampubolon@gmail.com>
2025-06-07 16:47:28 +0700

    redirect tag

--- src/middleware.js
+++ src/middleware.js
@@ -4,4 +4,7 @@ export function middleware(request) {
 	if (request.nextUrl.pathname.startsWith('/wordpress/wp-admin')) {
 		return NextResponse.rewrite(new URL('/wordpress/wp-admin', request.url))
 	}
+	if (request.nextUrl.pathname.startsWith('/wordpress/tag')) {
+		return NextResponse.rewrite(new URL('/'))
+	}
 }
